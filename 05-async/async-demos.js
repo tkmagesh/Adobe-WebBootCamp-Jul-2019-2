4a6a02ef2af9e7f75af219b68fad368958bbb756
Magesh <tkmagesh77@gmail.com>
2019-07-19 11:39:13 +0530

    bug data file

--- 05-async/async-demos.js
+++ 05-async/async-demos.js
@@ -14,19 +14,22 @@
 
 	window['addSyncClient'] = addSyncClient;
 
-	function addAsync(x,y){
+	function addAsync(x,y, callback){
 		console.log(`	[@Service] processing ${x} and ${y}`);
 		setTimeout(function(){
 				var result = x + y;
 			console.log(`	[@Service] returning result`);
-			return result;
+			callback(result);
+			console.log('job done');
 		}, 4000);
+
 	}
 
 	function addAsyncClient(x,y){
 		console.log(`[@Client] triggering the add service`);
-		var result = addAsync(x,y);
-		console.log(`[@Client] result = ${result}`);
+		addAsync(x,y, function(result){
+			console.log(`[@Client] result = ${result}`);
+		});
 	}
 
 	window['addAsyncClient'] = addAsyncClient;
